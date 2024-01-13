6ad20bf924acb0a0b153a8404bf29993ecb18be1
David <dtannyc1@gmail.com>
2024-01-12 23:44:36 -0500

    add basic react setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 3000,
+    historyApiFallback: true
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/, // was /\.js$/
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.jsx', '...']
+  }
+};
