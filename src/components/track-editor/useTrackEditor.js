e796ecfa424a53aafc8c756734cb724ae699f2bf
dtannyc1 <dtannyc1@gmail.com>
2026-03-26 18:12:47 -0400

    move track defaults to lib, move static image urls to imageLibrary

--- src/components/track-editor/useTrackEditor.js
+++ src/components/track-editor/useTrackEditor.js
@@ -1,57 +1,5 @@
 import { useState, useCallback } from 'react'
-
-export const createDefaultTrack = (name = 'New Track') => ({
-  name,
-  road: {
-    type: 'solid',
-    color: '#888888',
-    url: '',
-    dim: [1920, 1080],
-    scale: 1,
-    x: 0,
-    y: 0,
-  },
-  racingLine: {
-    url: 'https://www.dropbox.com/scl/fi/sp4n0j6iqbnpnme05zhak/racing_line.png?rlkey=rf7wga3zfnrz52z57i258vi1y&st=ihj77zey&dl=0',
-    dim: [200, 200],
-    scale: 1,
-    x: 1550,
-    y: 974,
-    p1: [1591, 1060],
-    p2: [1497, 948],
-    startModifiers: [
-      {
-        id: crypto.randomUUID(),
-        name: 'Start Flag',
-        url: 'https://www.dropbox.com/scl/fi/6sy2a7pvvuwkozk3tvbvq/start_flag.png?rlkey=ik03ay7yv17yv5bxk1lvhl39q&st=7juc9iog&dl=0',
-        dim: [200, 200],
-        scale: 1,
-        x: 1550,
-        y: 974,
-      },
-    ],
-    finishModifiers: [
-      {
-        id: crypto.randomUUID(),
-        name: 'Finish Flag',
-        url: 'https://www.dropbox.com/scl/fi/elrpti7l28qro4soudskz/finish_flag.png?rlkey=q8jsg8bt2dqzhqpj2litbmcsf&st=3iu1dexx&dl=0',
-        dim: [200, 200],
-        scale: 1,
-        x: 1550,
-        y: 974,
-      },
-    ],
-  },
-  scrollingImage: {
-    url: 'https://www.dropbox.com/scl/fi/hn1n4o8t737jxiqs5wse4/yellow_lines.png?rlkey=gxe6nyrkb66sblqoj1t8fnndr&st=83eyetrq&dl=0',
-    dim: [1920, 1080],
-    scale: 1,
-    x: 0,
-    y: -15,
-  },
-  backgroundAssets: [],
-  foregroundAssets: [],
-})
+import { createDefaultTrack } from '../../lib/trackDefaults'
 
 export const useTrackEditor = (initialTrack = null) => {
   const [track, setTrack] = useState(() => initialTrack ?? createDefaultTrack())
