ef7e5f0ed6e124e1698354fecb9c409a7b3bc3fb
dtannyc1 <dtannyc1@users.noreply.github.com>
2026-04-08 23:06:31 +0000

    deploy: 5c889beba8f3120a02883d4000dea8db0efc75e4

--- Game.js
+++ Game.js
@@ -107,7 +107,8 @@ class Game {
     }
 
     if (this.track.racingLine) {  
-      this.cameraLoc[0] = (this.track.racingLine.p1[0] + this.track.racingLine.p2[0]) / 2
+      const w = this.track.racingLine.dim[0] * this.track.racingLine.scale
+      this.cameraLoc[0] = (this.track.racingLine.p1[0] + this.track.racingLine.p2[0]) / 2 - w/2
     }
 
     this.track.resetScatteredArt(this.cameraLoc, this.canvas.width)
