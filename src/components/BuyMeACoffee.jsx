3f7ebb3e288e3897052f4b225a77e7a18161869d
dtannyc1 <dtannyc1@gmail.com>
2026-04-14 16:38:23 -0400

    change symbol

--- src/components/BuyMeACoffee.jsx
+++ src/components/BuyMeACoffee.jsx
@@ -3,7 +3,7 @@ const BuyMeACoffee = ({py = 2, px = 4}) => {
   return (
     <a href="https://www.buymeacoffee.com/pencils45" target="_blank" className="relative">
       <span className={`font-cookie text-center text-black text-xl bg-[#FFDD00] py-${py} px-${px} font-semibold rounded-md `}>
-        💵 Support the project
+        💎 Support the project
       </span>
     </a>
   )
