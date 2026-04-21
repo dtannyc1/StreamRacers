84ee26b9812c5debb30a87ddfd209d5995392e0a
dtannyc1 <dtannyc1@gmail.com>
2026-04-21 13:20:39 -0400

    restyle

--- public/Game.js
+++ public/Game.js
@@ -184,8 +184,10 @@ class Game {
           white-space: nowrap;
           overflow: hidden;
           text-overflow: ellipsis;
+          min-height: 1.2em;
         }
         .finished { color: cyan; }
+        .hidden { display: none; }
     `)
 
     document.adoptedStyleSheets = [sheet]
@@ -498,6 +500,7 @@ class Game {
       
       // Hide row if no data, or update it
       if (i >= this.standings.length) {
+        row.classList.toggle('finished', true)
         continue;
       }
       
